1fec86f8f32fe5c83c54463a371005974ee19e6b
kuruva-phaneendra <kuruvaphaneendra@gmail.com>
2025-01-16 17:32:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=Tru
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
-    st.write(my_insert_stmt)
+st.write(my_insert_stmt)
     #st.stop()
     time_to_insert = st.button('Submit Order')
     
